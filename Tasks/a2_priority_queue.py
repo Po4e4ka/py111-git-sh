31ace5de3c95e3ccfbd511649d60b8dacc291819
Алексей <toolset@live.ru>
2020-05-23 12:49:33 +0300

    a2 all test passed

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -6,6 +6,9 @@ Queue priorities are from 0 to 5
 from typing import Any
 
 
+my_priority_queue = []
+
+
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
     Operation that add element to the end of the queue
@@ -13,6 +16,8 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
+    global  my_priority_queue
+    my_priority_queue.append({"priority": priority, "elem": elem})
     return None
 
 
@@ -22,7 +27,17 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
-    return None
+    temp = 0
+    global my_priority_queue
+    if len(my_priority_queue) < 1:
+        return None
+    result = my_priority_queue[0]
+    for i in range(1,len(my_priority_queue)):
+        if result["priority"] > my_priority_queue[i]["priority"]:
+            result = my_priority_queue[i]
+            temp = i
+    del my_priority_queue[temp]
+    return result["elem"]
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -32,7 +47,11 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None
+    global my_priority_queue
+    if ind > len(my_priority_queue):
+        return None
+    else:
+        return my_priority_queue[ind]["elem"]
 
 
 def clear() -> None:
@@ -41,4 +60,13 @@ def clear() -> None:
 
     :return: None
     """
+    global my_priority_queue
+    my_priority_queue = []
     return None
+
+if __name__ == "__main__":
+    for i in range(10,20):
+        enqueue(i, 2)
+    for i in range(10):
+        enqueue(i, 0)
+    print(dequeue())
