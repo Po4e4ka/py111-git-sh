6ec3b837133befa2b13497e3d4bc902b0c5f16ac
Po4e4ka <toolset@live.ru>
2020-05-30 11:24:48 +0300

    с0 is ended. all test's passed

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,6 +5,8 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
+    if n < 1:
+        raise ValueError('n < 0')
     if n == 1 or n == 2:
         return 1
     return fib_recursive(n-1)+fib_recursive(n-2)
@@ -17,6 +19,8 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
+    if n < 1:
+        raise ValueError('n < 0')
     result = [1,1]
     for i in range(3,n+1):
         result[0], result[1] = result[1], result[0] + result[1]
