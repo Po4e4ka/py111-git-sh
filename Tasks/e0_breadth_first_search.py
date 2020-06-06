7d59aabcd2c7cfb3f4e26d424ff856eed46cf070
Алексей <toolset@live.ru>
2020-06-06 13:41:24 +0300

    e0 ended, all tests passsed

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+import Tasks.a1_my_queue as queue
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +11,16 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visible = [start_node]
+    queue.enqueue(start_node)
+    while True:
+        versh = queue.dequeue()
+        if versh is None:
+            break
+        list1 = g.neighbors(versh)
+        for i in list1:
+            if visible.count(i) == 0:
+                visible.append(i)
+                queue.enqueue(i)
+
+    return visible
