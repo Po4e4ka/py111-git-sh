ac9bc354d4208002fffbb1bb79dcdb73fe683718
Алексей <toolset@live.ru>
2020-06-06 11:22:09 +0300

    d0 ended. all test passed

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,8 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    result = [stairway[0], stairway[1]]
+    i = 0
+    for i in range(2,len(stairway)):
+        result.append(stairway[i]+min(result[i-1],result[i-2]))
+    return result[-1]
