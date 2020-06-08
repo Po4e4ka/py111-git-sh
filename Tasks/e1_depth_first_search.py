f67dca12414be10842595ca1b92b1b93bb3fcee6
Алексей <toolset@live.ru>
2020-06-08 19:43:27 +0300

    e1 is ended. all tests passed

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,6 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
-
+import Tasks.a0_my_stack as stk
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     """
@@ -10,5 +10,21 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visible = [start_node]
+    for i in visible:
+        list1 = list(g.neighbors(i))
+        bol = 0  # Флаг проверки добавления в висибл
+        for j in list1:
+            if visible.count(j) == 0:
+                if bol == 0:  # Если уже добавили элемент в визибл
+                    bol = 1
+                    visible.append(j)
+                else:  # все остальные в стэк
+                    stk.push(j)
+        if len(list1) == 1:  # Если из соседей только предыдущий элемент
+            x = stk.pop()    # то берем элемент из стэка
+            if x is None:    # Если стэк пустой, то завершаем цикл - все элементы найдены
+                break
+            if visible.count(x) == 0:
+                visible.append(x)
+    return visible
